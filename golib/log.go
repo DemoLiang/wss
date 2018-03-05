b27399660e7e3b5047f07423fc91173d4e1406ec
Demo <vimliang@gmail.com>
2018-03-05 23:03:22 +0800

    增加部分游戏规则

--- golib/log.go
+++ golib/log.go
@@ -3,9 +3,5 @@ package golib
 import "fmt"
 
 func Log(format string, a ...interface{}) (n int, err error) {
-	//if a != nil{
-		return fmt.Printf(format,a...)
-	//}else {
-	//	return fmt.Printf(format,nil)
-	//}
+	return fmt.Printf(format, a...)
 }
