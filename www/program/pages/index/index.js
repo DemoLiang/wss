8b61248c30faad9eb48d6161a8109efe187d569e
vimliang@gmail.com <vimliang@gmail.com>
2018-04-10 15:05:09 +0800

    修改小程序部分代码测试

--- www/program/pages/index/index.js
+++ www/program/pages/index/index.js
@@ -7,43 +7,36 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    code: '',
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    code: app.globalData.code,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    sssssss:''
   },
   //事件处理函数
   bindViewTap: function() {
     wx.connectSocket({
       url: 'ws://127.0.0.1:8888/ws',
-      method:"GET",
+      method:"POST",
       success:res => {
         console.log("aaaaaaaaaaaa")
       }
     })
     wx.onSocketOpen(function (res) {
       console.log('WebSocket连接已打开！')
+      // var that = this;
       wx.login({
-        success: function (res) {
+        success: res=> {
           if (res.code) {
             //发起网络请求
             app.globalData.code = res.code
-            wx.sendSocketMessage({
-              data:  res.code
-            })
           } else {
             console.log('获取用户登录态失败！' + res.errMsg)
           }
         }
       });
-      // wx.sendSocketMessage({
-      //   data: 'Hello,World:' + Math.round(Math.random() * 0xFFFFFF).toString(),
-      // })
       wx.onSocketMessage(function (res) {
         console.log('收到服务器内容：' + res.data)
       })
     })
-    // wx.navigateTo({
-    //   url: '../logs/logs'
-    // })
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -80,32 +73,26 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-    // wxRequest
   },
-  // wxRequest:function(e){
-  //   wx.request({
-  //   url: "127.0.0.1:7777/ws",
-  //   data: "aaaaaaaa",
-  //   method: "POST",
-  //   dataType: "json",
-  //   success:res =>{
-  //       console.log('aaaaaaaaaa')
-  //     }
-  //   })
-  // }
   bindFormSubmit: function (e) {
     console.log(e.detail.value.textarea)
+    var that = this
     wx.sendSocketMessage({
       data: JSON.stringify({
         message_type:1,
-         content :e.detail.value.textarea,
-         code: app.globalData.code
-         }
-        ),
-      // code:dataContent.code
+        content :e.detail.value.textarea,
+        code: app.globalData.code,
+        abc: e.detail.value.land,
+        }),
       success: function (res) { },
       fail: function (res) { },
       complete: function (res) { },
+    })
+    wx.onSocketMessage(function(res){
+      console.log('收到服务器内容：' + res.data)
+      that.setData({
+        sssssss: res.data.Session,
+      })
     })
   },
 })
